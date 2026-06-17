b22f21beed5677805bbf3d0f394e6387a23f6136
Nguyen Dang Phuong Uyen <phuongyeng1709@gmail.com>
2026-06-17 13:24:12 +0700

    backend

--- backend/src/main/java/com/nguyenlengocduy/smartbanking/backend/BackendApplication.java
+++ backend/src/main/java/com/nguyenlengocduy/smartbanking/backend/BackendApplication.java
@@ -1,7 +1,7 @@
 package com.nguyenlengocduy.smartbanking.backend;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplica
 
 @SpringBootApplication
 public class BackendApplication {
@@ -9,5 +9,10 @@ public class BackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
-
+    public class Account {
+        private long AccountNumber;
+        private String AccountName;
+        private double Balance;
+        Scanner sc = new Scanner(System.in);
+    }
 }
